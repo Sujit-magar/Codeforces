a05df71024066e67f238632cb66724cdae50692d
Sujit Magar <98046388+Sujit-magar@users.noreply.github.com>
2022-11-28 16:48:53 +0530

    Update problemA.cpp

--- Contests/Division/Codeforces Round #835 (Div. 4)/problemA.cpp
+++ Contests/Division/Codeforces Round #835 (Div. 4)/problemA.cpp
@@ -1,6 +1,29 @@
 #include<iostream>
 using namespace std;
+
+void swap(int* xp, int* yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+void bubbleSort(int a[], int n)
+{
+    int i, j;
+    for (i = 0; i < n - 1; i++)
+        for (j = 0; j < n - i - 1; j++)
+            if (a[j] > a[j + 1])
+                swap(&a[j], &a[j + 1]);
+}
 int main(){
-    cout<<"Hello"<<endl;
+    int t; cin>>t;
+    while(t--){
+        int a[3];
+        for(int i=0;i<3;i++){
+            cin>>a[i];
+        }
+        bubbleSort(a, 3);
+        cout<<a[1]<<endl;
+    }
     return 0;
 }
